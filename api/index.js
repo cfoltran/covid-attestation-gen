790a648890c76daaa2c2b3eb729df34f253a6d62
owalid <othmane.el.ayadi@gmail.com>
2020-10-31 04:38:21 +0100

    finish !

--- api/index.js
+++ api/index.js
@@ -21,7 +21,20 @@ app.use(helmet())
 // Export express app
 module.exports = app
 
-app.post('/certificate', async (req, res) => await certificate(req, res))
+const deletePdfFile = (req, res, next) => {
+  const fs = require('fs')
+  const { hash } = req.body
+  const dir = `./my-downloads/${hash}`
+  fs.mkdirSync(dir, { recursive: true })
+  const fileNames = fs.readdirSync(dir)
+  fileNames.map((item) => {
+    fs.unlinkSync(`${dir}/${item}`)
+    console.log('fileNames', item)
+  })
+  next()
+}
+
+app.post('/certificate', (deletePdfFile), async (req, res) => await certificate(req, res))
 
 if (process.env.NODE_ENV === 'prod') {
   app.use((req, res, next) => {
@@ -52,13 +65,17 @@ const certificate = async (req, res) => {
       city,
       zipcode,
       hour,
-      reason
+      reason,
+      hash
     } = req.body
     const browser = await puppeteer.launch({
       headless: true,
       args: ['--lang=fr-FR,fr']
     })
+    const dir = `./my-downloads/${hash}`
+    const fs = require('fs')
     const page = await browser.newPage()
+
     await page.setViewport({ width: 1280, height: 800 })
     await page.goto('https://media.interieur.gouv.fr/deplacement-covid-19')
     await page.type('#field-firstname', `${firstname}`)
@@ -71,20 +88,16 @@ const certificate = async (req, res) => {
     await page.type('#field-heuresortie', `${hour}`)
     await page.click(`input[value=${reason}]`)
     await page.click('button[id=generate-btn]')
+    await page._client.send('Page.setDownloadBehavior', { behavior: 'allow', downloadPath: dir })
     await page.waitForFunction('document.querySelectorAll("a").length > 8')
     const pdfLink = await page.evaluate(() => {
       const links = document.querySelectorAll('a')
       return links[8].href
     })
-    // const finalResponse = await page.waitForResponse(response => response);
-    // console.log(finalResponse);
-    // let responseJson = await finalResponse.json();
-    // console.log(responseJson);
-    // console.log('Blob?', ))
-    console.log(pdfLink)
-    res.status(200).json({ link: pdfLink })
-    // res.setHeader('Content-Type', 'application/pdf');
-    // res.sendFile(new Blob([pdfLink], { type: 'application/pdf' }) );
+    await page.waitFor(1000)
+    const fileNames = fs.readdirSync(dir)
+    const fileData = fs.readFileSync(`${dir}/${fileNames[0]}`, 'base64')
+    res.status(200).json({ link: pdfLink, fileData })
   } catch (error) {
     console.error(error)
     res.status(200).json({ error })
