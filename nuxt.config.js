c3187138cdd98ece098307441f7c8ecade5933d9
owalid <othmane.el.ayadi@gmail.com>
2020-11-04 01:29:03 +0100

    delete server property

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,11 +66,6 @@ export default {
     maxAge: 2147483647
   },
 
-  server: {     
-    port: 3000, // default: 3000     
-    host: '0.0.0.0', // default: localhost   
-  },
-
   /*
   ** Auto import components
   ** See https://nuxtjs.org/api/configuration-components
